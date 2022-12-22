98aba3d01c1b40d44aaa5d9690d058ceee5d99e4
mustafa mehel <mehelx5207@gmail.com>
2022-12-22 11:32:05 +0300

    asa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import React from "react";
+import { useGlobalContext } from "./context";
+
+// components
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+// items
 
 function App() {
-  return <div className="App"></div>;
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 
 export default App;
